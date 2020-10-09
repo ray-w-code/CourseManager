b85a493c342bb81bf474e1c52266d6b23edb65c9
Raymond Wan <rwan615@gmail.com>
2020-10-08 17:05:10 -0700

    first commit, set up App

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class CoursesPage extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <h2>Courses</h2>;
+  }
+}
+export default CoursesPage;
